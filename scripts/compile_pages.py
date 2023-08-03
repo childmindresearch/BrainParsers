ea7fdc3244f634f78d6df6b852cab8b91c4e72c8
Florian Rupprecht <floruppr@gmail.com>
2023-08-03 19:04:44 -0400

    Reactive

--- scripts/compile_pages.py
+++ scripts/compile_pages.py
@@ -17,6 +17,7 @@ HTML_TEMPLATE = """
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>BrainParsers</title>
     <style>
         body {{
